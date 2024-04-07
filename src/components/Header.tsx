0f20e4903e5710960fd1432e6a54995d1ef69445
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-04-07 17:16:02 +0100

    Introduced Header Component. Worked on Desktop Navigation.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,57 @@
 'use client';
 
+import Link from 'next/link';
+import headerNavigationLinks from '@/data/headerNavigationLinks';
+import { usePathname } from 'next/navigation';
+import ThemeSwitch from '@/components/ThemeSwitch';
+import MobileNavigation from '@/components/navigation/MobileNavigation';
+
 export const Header = () => {
-  return <div className="mx-auto max-w-3xl px-4 sm:px-9 xl:max-w-5xl xl:px-0"></div>;
+  const pathName = usePathname();
+
+  return (
+    <div className="mx-auto max-w-3xl px-4 sm:px-9 xl:max-w-5xl xl:px-0">
+      <header className="z-40 bg-transparent py-5 md:py-10">
+        <div className="mx-auto flex max-w-5xl items-center justify-between">
+          <div>
+            <Link
+              href="/"
+              className={`horizontal-underline hidden text-3xl font-extrabold sm:block ${
+                pathName === '/' ? 'horizontal-underline-active' : ''
+              }`}
+              aria-label="W."
+            >
+              W.
+            </Link>
+          </div>
+          <div className="flex items-center space-x-3 text-base leading-5">
+            <div className="hidden space-x-5 sm:flex">
+              {headerNavigationLinks.map(({ title, href }) => {
+                const active = pathName?.includes(href);
+                return (
+                  <Link
+                    prefetch
+                    key={title}
+                    href={href}
+                    aria-label={title}
+                    className={`horizontal-underline text-base ${
+                      active ? 'horizontal-underline-active' : ''
+                    }`}
+                  >
+                    <span className="font-semibold tracking-wide text-gray-900 dark:text-gray-100">
+                      {title}
+                    </span>
+                  </Link>
+                );
+              })}
+            </div>
+            <div className="flex items-center">
+              <ThemeSwitch />
+              <MobileNavigation />
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 };

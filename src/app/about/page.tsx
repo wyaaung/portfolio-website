35da69c94d38c57bc6f38c7fd8d2a33013118870
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-04-07 23:58:59 +0100

    Some refactorization

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Layout from '@/components/layouts/Layout';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Layout>
+      <div>About</div>
+    </Layout>
+  );
 };
 
 export default About;
---
+++ src/components/wrappers/Container.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Container = ({ children }: Props) => {
+  return <div className="mx-auto max-w-3xl px-4 sm:px-9 xl:max-w-5xl xl:px-0">{children}</div>;
+};
+
+export default Container;

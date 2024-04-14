4c18a8e74e75c2b7b7581b266196c948c65cdb67
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-04-14 03:43:17 +0100

    Installed ContentLayer

---
+++ contentlayer.config.ts
@@ -0,0 +1,65 @@
+import { ComputedFields, defineDocumentType, makeSource } from 'contentlayer2/source-files';
+import readingTime from 'reading-time';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypePrismPlus from 'rehype-prism-plus';
+import rehypeSlug from 'rehype-slug';
+import remarkCodeTitles from './src/lib/remark-code-title';
+import { extractTocHeadings } from './src/lib/remark-toc-headings';
+
+
+const computedFields: ComputedFields = {
+  readingTime: { type: 'json', resolve: (doc) => readingTime(doc.body.raw) },
+  slug: {
+    type: 'string',
+    resolve: (doc) => doc._raw.flattenedPath.replace(/^.+?(\/)/, ''),
+  },
+  toc: { type: 'string', resolve: (doc) => extractTocHeadings(doc.body.raw) },
+};
+
+export const Author = defineDocumentType(() => ({
+  name: 'Author',
+  filePathPattern: 'blog/*.mdx',
+  contentType: 'mdx',
+  fields: {
+    name: { type: 'string', required: true },
+    avatar: { type: 'string' },
+    occupation: { type: 'string' },
+    company: { type: 'string' },
+    email: { type: 'string' },
+    twitter: { type: 'string' },
+    linkedin: { type: 'string' },
+    github: { type: 'string' },
+    layout: { type: 'string' },
+  },
+  computedFields
+}));
+
+export const Blog = defineDocumentType(() => ({
+  name: 'Blog',
+  filePathPattern: 'blog/**/*.mdx',
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+    tags: { type: 'list', of: { type: 'string' } },
+    lastmod: { type: 'date' },
+    draft: { type: 'boolean' },
+    summary: { type: 'string' },
+    images: { type: 'list', of: { type: 'string' } },
+    author: { type: 'string', required: true },
+    layout: { type: 'string' },
+    bibliography: { type: 'string' },
+    canonicalUrl: { type: 'string' },
+  },
+  computedFields,
+}));
+
+export default makeSource({
+  contentDirPath: 'content',
+  documentTypes: [Blog, Author],
+  mdx: {
+    cwd: process.cwd(),
+    remarkPlugins: [remarkCodeTitles],
+    rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, [rehypePrismPlus, { ignoreMissing: true }]],
+  },
+});

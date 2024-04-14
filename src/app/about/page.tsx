6ad6de3983ca9c0d8da4d1fe3c78f82b5e5516a5
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-04-14 04:12:23 +0100

    First MDX Added for Portfolio

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import MainLayout from '@/components/layouts/MainLayout';
 import AuthorLayout from '@/components/layouts/AuthorLayout';
+import { allAuthors } from '../../../.contentlayer/generated';
 
 const About = () => {
+  const author = allAuthors.find((p) => p.slug === 'about');
+
+  if (!author) {
+    return null;
+  }
+
   return (
     <MainLayout>
-      <AuthorLayout>
+      <AuthorLayout content={author}>
         <div>ABOUT</div>
       </AuthorLayout>
     </MainLayout>
--- src/components/layouts/AuthorLayout.tsx
+++ src/components/layouts/AuthorLayout.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import Image from 'next/image';
+import type { Author } from 'contentlayer/generated';
 
 interface Props {
   children: React.ReactNode;
+  content: Omit<Author, '_id' | '_raw' | 'body'>;
 }
 
-const AuthorLayout = ({ children }: Props) => {
+const AuthorLayout = ({ children, content }: Props) => {
+  console.log(content);
+  const { avatar, occupation, company } = content;
+
   return (
     <div className="pt-8">
       <div className="mb-8 flex flex-col-reverse items-center justify-between sm:flex-row sm:items-center">
         <div className="text-center sm:text-left">
           <h1 className="text-xl font-bold md:text-3xl lg:text-4xl">William (Wai Yan) Aung</h1>
           <h2 className="text-sm font-normal md:text-base">
-            Software Engineer <span className="font-semibold">THG Plc.</span>
+            {occupation} at <span className="font-semibold">{company}</span>
           </h2>
         </div>
-        <div>Image</div>
+        <div>
+          <Image
+            alt="William (Wai Yan) Aung"
+            height={130}
+            width={130}
+            src={avatar || ''}
+            className="rounded-full object-scale-down grayscale-0"
+          />
+        </div>
       </div>
       <div className="prose max-w-none pb-8 text-justify text-sm dark:prose-dark md:text-lg xl:col-span-2">
         {children}

c968e638506e9e20389b907323d3b2f8343567ad
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-04-14 15:47:49 +0100

    Added Metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,11 @@ import AuthorLayout from '@/layouts/AuthorLayout';
 import { allAuthors } from 'contentlayer/generated';
 import MDXLayoutRenderer from '@/components/MDXComponents';
 
+export const metadata = {
+  title: 'About - William (Wai Yan Aung)',
+  description: 'About me - William (Wai Yan Aung)',
+};
+
 const About = () => {
   const author = allAuthors.find((p) => p.slug === 'about');
 
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import MainLayout from '@/layouts/MainLayout';
 
+export const metadata = {
+  title: 'About - William (Wai Yan Aung)',
+  description: 'About me - William (Wai Yan Aung)',
+};
+
 const Blogs = () => {
   return (
     <MainLayout>

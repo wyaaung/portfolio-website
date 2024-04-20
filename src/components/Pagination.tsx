0e4568240babf02abef59d678b884509d51590fb
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-04-20 23:26:09 +0100

    Included pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import React from 'react';
+
+interface Props {
+  totalPages: number;
+  currentPage: number;
+}
+
+const Pagination = ({ totalPages, currentPage }: Props) => {
+  const prevPage = currentPage - 1 > 0;
+  const nextPage = currentPage + 1 <= totalPages;
+
+  return (
+    <div className="space-y-2 md:space-y-5">
+      <nav className="flex justify-between">
+        {!prevPage && (
+          <button className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
+            Previous
+          </button>
+        )}
+        {prevPage && (
+          <Link
+            href={currentPage - 1 === 1 ? '/blogs/' : `/blogs/page/${currentPage - 1}`}
+            className="underline-magical"
+          >
+            <button>Previous</button>
+          </Link>
+        )}
+        <span>
+          {currentPage} of {totalPages}
+        </span>
+        {!nextPage && (
+          <button className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
+            Next
+          </button>
+        )}
+        {nextPage && (
+          <Link href={`/blogs/page/${currentPage + 1}`} className="underline-magical">
+            <button>Next</button>
+          </Link>
+        )}
+      </nav>
+    </div>
+  );
+};
+
+export default Pagination;

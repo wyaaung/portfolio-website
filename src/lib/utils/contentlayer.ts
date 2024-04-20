72914cc1fe24654518dbd752de00a7708451eb26
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-04-20 21:10:46 +0100

    Put some comments for contentlayer methods

--- src/lib/utils/contentlayer.ts
+++ src/lib/utils/contentlayer.ts
@@ -1,4 +1,14 @@
-import type { Blog, DocumentTypes } from 'contentlayer/generated';
+import type { Document, MDX } from 'contentlayer2/core';
+
+export type MDXDocument = Document & { body: MDX };
+export type MDXDocumentDate = MDXDocument & {
+  date: string;
+};
+
+export type MDXBlog = MDXDocumentDate & {
+  tags?: string[];
+  draft?: boolean;
+};
 
 export function dateSortDesc(a: string, b: string) {
   if (a > b) return -1;
@@ -6,10 +16,25 @@ export function dateSortDesc(a: string, b: string) {
   return 0;
 }
 
-export function sortedBlogPost(allBlogs: Blog[]) {
-  return allBlogs.sort((a, b) => dateSortDesc(a.date, b.date)).filter((p) => p.draft === false);
+/**
+ * Sorts a list of MDX documents by date in descending order
+ *
+ * @param {MDXDocumentDate[]} allBlogs
+ * @param {string} [dateKey='date']
+ * @return {*}
+ */
+export function sortBlogs<T extends MDXDocumentDate>(allBlogs: T[], dateKey: string = 'date') {
+  return allBlogs.sort((a, b) => dateSortDesc(a[dateKey], b[dateKey]));
 }
 
+/**
+ * A typesafe omit helper function
+ * @example omit(content, ['body', '_raw', '_id'])
+ *
+ * @param {Obj} obj
+ * @param {Keys[]} keys
+ * @return {*}  {Omit<Obj, Keys>}
+ */
 export const omit = <Obj, Keys extends keyof Obj>(obj: Obj, keys: Keys[]): Omit<Obj, Keys> => {
   const result = Object.assign({}, obj);
   keys.forEach((key) => {
@@ -20,6 +45,12 @@ export const omit = <Obj, Keys extends keyof Obj>(obj: Obj, keys: Keys[]): Omit<
 
 export type CoreContent<T> = Omit<T, 'body' | '_raw' | '_id'>;
 
-export function coreContent<T extends DocumentTypes>(content: T) {
+/**
+ * Omit body, _raw, _id from MDX document and return only the core content
+ *
+ * @param {T} content
+ * @return {*}  {CoreContent<T>}
+ */
+export function coreContent<T extends MDXDocument>(content: T): CoreContent<T> {
   return omit(content, ['body', '_raw', '_id']);
 }

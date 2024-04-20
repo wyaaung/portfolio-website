0e7e36f38a5e0d9d6b086d800939dd4ad3f119ae
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-04-20 22:26:19 +0100

    Added a comment and changes to proper handling to get TableOfContent

--- src/lib/remark-toc-headings.ts
+++ src/lib/remark-toc-headings.ts
@@ -2,16 +2,19 @@ import GithubSlugger from 'github-slugger';
 import { Heading } from 'mdast';
 import { toString } from 'mdast-util-to-string';
 import { remark } from 'remark';
-import { Toc } from '@/types/Toc';
+import { TableOfContent } from '@/types/TableOfContent';
 import { Parent } from 'unist';
 import { visit } from 'unist-util-visit';
 import { VFile } from 'vfile';
 
 const slugger = new GithubSlugger();
 
+/**
+ * Extracts TOC headings from markdown file and adds it to the file's data object.
+ */
 export function remarkTocHeadings() {
   return (tree: Parent, file: VFile) => {
-    const toc: Toc = [];
+    const toc: TableOfContent = [];
     visit(tree, 'heading', (node: Heading) => {
       const textContent = toString(node);
       toc.push({
@@ -24,7 +27,14 @@ export function remarkTocHeadings() {
   };
 }
 
-export async function extractTocHeadings(markdown: string) {
+
+/**
+ * Passes markdown file through remark to extract TOC headings
+ *
+ * @param {string} markdown
+ * @return {*}  {Promise<Toc>}
+ */
+export async function extractTocHeadings(markdown: string): Promise<TableOfContent> {
   const vfile = await remark().use(remarkTocHeadings).process(markdown);
   return vfile.data.toc;
 }

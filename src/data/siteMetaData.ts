967ffdf0f0e3bad45b5aec1fedfa73ea7b7dc0bc
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-04-20 21:14:14 +0100

    Updated locale and language

--- src/data/siteMetaData.ts
+++ src/data/siteMetaData.ts
@@ -3,8 +3,8 @@ export const siteMetaData = {
   author: 'William (Wai Yan) Aung',
   headerTitle: 'William (Wai Yan) Aung',
   description: 'Software Engineer at THG',
-  language: 'en-us',
-  locale: 'en-US',
+  language: 'en-gb',
+  locale: 'en-GB',
   theme: 'light', // system, dark or light
   linkedin: 'https://www.linkedin.com/in/wyaaung/',
   github: 'https://github.com/wyaaung',

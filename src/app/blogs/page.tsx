30f4c0365296e91b8b2c0dfa954d86437565c8ca
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-04-20 23:26:51 +0100

    Implemented pagination logic for list of blogs site

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -2,18 +2,31 @@ import React from 'react';
 import MainLayout from '@/layouts/MainLayout';
 import ListLayout from '@/layouts/mdx/ListLayout';
 import { allBlogs } from 'contentlayer/generated';
+import { sortBlogs } from '@/lib/utils/contentlayer';
+import { BLOGS_PER_PAGE } from '@/types/constants';
 
 export const metadata = {
-  title: 'About - William (Wai Yan Aung)',
-  description: 'About me - William (Wai Yan Aung)',
+  title: 'Blogs - William (Wai Yan Aung)',
+  description: 'My Blogs - William (Wai Yan Aung)',
 };
 
 const Blogs = () => {
-  const activeBlogs = allBlogs.filter((p) => p.draft === false);
+  const activeBlogs = allBlogs.filter((blog) => blog.draft === false);
+  const blogs = sortBlogs(activeBlogs);
+  const initialDisplayBlogs = blogs.slice(0, BLOGS_PER_PAGE);
+  const pagination = {
+    currentPage: 1,
+    totalPages: Math.ceil(blogs.length / BLOGS_PER_PAGE),
+  };
 
   return (
     <MainLayout>
-      <ListLayout blogs={activeBlogs} title="Blogs" />
+      <ListLayout
+        blogs={activeBlogs}
+        initialDisplayBlogs={initialDisplayBlogs}
+        pagination={pagination}
+        title="Blogs"
+      />
     </MainLayout>
   );
 };
--- src/layouts/mdx/ListLayout.tsx
+++ src/layouts/mdx/ListLayout.tsx
@@ -3,35 +3,73 @@
 import React from 'react';
 import { CoreContent } from '@/lib/utils/contentlayer';
 import { Blog } from 'contentlayer/generated';
-import BlogCard from '../../components/blog/BlogCard';
+import BlogCard from '@/components/blog/BlogCard';
+import Pagination from '@/components/Pagination';
 
 interface Props {
   blogs: CoreContent<Blog>[];
   title: string;
   initialDisplayBlogs?: CoreContent<Blog>[];
+  pagination?: React.ComponentProps<typeof Pagination>;
 }
 
-const ListLayout = ({ blogs, title, initialDisplayBlogs = [] }: Props) => {
+const ListLayout = ({ blogs, title, initialDisplayBlogs = [], pagination }: Props) => {
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const filteredBlogPosts = blogs.filter((blog) => {
+    const searchContent = blog.title + blog.summary + blog.tags?.join(' ');
+    return searchContent.toLowerCase().includes(searchValue.toLowerCase());
+  });
+
+  const displayBlogs =
+    initialDisplayBlogs.length > 0 && !searchValue ? initialDisplayBlogs : filteredBlogPosts;
+
   return (
-    <div>
+    <>
       <div className="space-y-2 rounded-lg pt-8 pb-3 md:space-y-5">
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
           {title}
         </h1>
-      </div>
-      <ul>
-        {blogs.map(({ slug, title, tags, summary }, index) => (
-          <BlogCard
-            key={slug}
-            title={title}
-            slug={slug}
-            summary={summary}
-            tags={tags}
-            index={index}
+        <div className="relative max-w-full">
+          <input
+            aria-label="Search articles"
+            type="text"
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search articles"
+            className="block w-full rounded-md border-0 bg-gray-200 bg-opacity-50 px-4 py-3 text-gray-900 focus:border-sky-500 focus:ring-sky-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
           />
-        ))}
-      </ul>
-    </div>
+          <svg
+            className="absolute right-3 top-3 h-6 w-6 text-gray-400 dark:text-gray-300"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </div>
+        <ul>
+          {displayBlogs.map(({ slug, title, tags, summary }, index) => (
+            <BlogCard
+              key={slug}
+              title={title}
+              slug={slug}
+              summary={summary}
+              tags={tags}
+              index={index}
+            />
+          ))}
+        </ul>
+      </div>
+      {pagination && pagination.totalPages > 1 && !searchValue && (
+        <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} />
+      )}
+    </>
   );
 };
 
